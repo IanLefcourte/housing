2c8ce48f6075509b894bb9fb45301ff2ceead9cd
Andy Hull <andyhull3@gmail.com>
2013-03-18 19:54:23 -0700

    basic framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Housing::Application.config.secret_token = '45089210cda77cb1e3acfc002c6422f294f07559781f8391f9730df643ed154141b754dd05227b2f11aa2234438f9f02c5aa1893017bf88b268a13aaf65416df'
