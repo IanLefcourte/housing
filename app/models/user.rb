e1e41be8c5a2ca0e3bc286fe6c3a387d87a6290f
Andy Hull <andyhull3@gmail.com>
2013-03-19 12:14:04 -0700

    validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,14 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :lists_id
+  attr_accessible :email, :name, :lists_id, :password, :password_confirmation
+  has_secure_password
   has_and_belongs_to_many :list
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 end
 
